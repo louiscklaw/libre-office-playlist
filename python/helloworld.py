51b5449ce9e6a8d3b32718c04d8f0e98c6cc1036
louiscklaw <louiscklaw@gmail.com>
2020-10-30 19:12:56 +0800

    update,

---
+++ python/helloworld.py
@@ -0,0 +1,24 @@
+import json
+import os,sys
+from pprint import pprint
+
+import yfinance as yf
+
+sys.path.append('/home/logic/.config/libreoffice/4/user/Scripts/python')
+sys.dont_write_bytecode = True
+
+from test_func_return import *
+
+def helloworld():
+  ctx = XSCRIPTCONTEXT.getComponentContext()
+  serviceManager = ctx.ServiceManager
+  doc = XSCRIPTCONTEXT.getDocument()
+
+  sheets = doc.getSheets() #XSpreadSheets
+  status_sheet = sheets.getByName('status')
+  helloworld_sheet = sheets.getByName('helloworld')
+
+  xCell = status_sheet.getCellByPosition(1, 3)
+  temp = self_test()
+  xCell.setString('')
+  xCell.setString(temp)
